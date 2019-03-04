b208c450584185f2ea927145e9c044cc45440c84
Soilhat Mohamed <soilhatmohamed@gmail.com>
2019-03-04 22:28:53 +0100

    starwars tests suited

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,37 +7,32 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      var strings = true;
       starWars.all.forEach( function(name){
-        strings = (typeof name == "string");
+        expect(typeof name == "string");
       });
-      expect(strings);
     });
 
     test('should contain `Luke Skywalker`', () => {
-      var strings = true;
       starWars.all.forEach( function(name){
-        strings = name.includes('Luke Skywalker');
+        expect(name.includes('Luke Skywalker'));
       });
-      expect(strings);
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      var strings = true;
       starWars.all.forEach( function(name){
-        strings = !name.includes('Ben Quadinaros');
+        expect(!name.includes('Ben Quadinaros'));
       });
-      expect(strings);
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.random(undefined)).not.toMatch(starWars.random(undefined));
+      expect(starWars.all.includes(starWars.random(undefined)));
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.random(1)[0]).not.toMatch(starWars.random(1)[0]);
     });
   });
-});
+})
